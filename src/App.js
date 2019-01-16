56bcd8a3a13ea9a7f3eb2ca0962d1992e0207209
timoxin.artur <timoxin.artur@gmail.com>
2019-01-16 16:09:10 +0200

    add dynamic grid

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import Target from "./components/target/Target";
 class App extends Component {
   state = {
     targets: [],
-    countOfColumnsAndRows: 5,
   };
 
   componentDidMount() {
     let startTargets = [];
-    for (let i = 0; i < this.state.countOfColumnsAndRows; i++) {
+    // сначала сетка 5x5
+    for (let i = 0; i < 5; i++) {
       startTargets.push([]);
-      for (let j = 0; j < this.state.countOfColumnsAndRows; j++) {
+      for (let j = 0; j < 5; j++) {
         startTargets[i].push(0);
       }
     }
@@ -24,26 +24,28 @@ class App extends Component {
     if (column < 0 || row < 0) {
       console.log("Target is not exist");
       return false;
-    } else if (row > this.state.countOfColumnsAndRows - 1) {
+    } else if (row > this.state.targets.length - 1) {
       // -1 т.к. нач. и все послед массив у нас 0 1 2 3 4 - колонок
       //добавляем колонку путём добавляения каждому из масиивов еще одного значение
       console.log("add new row");
       let newRow = [];
-      for (let i = 0; i < this.state.countOfColumnsAndRows; i++) {
+      for (let i = 0; i < this.state.targets[0].length; i++) {
         newRow.push(0); // заполняем новую строку нулями
       }
       console.log(newRow);
       newTargets.push(newRow); // добавляем новую строку
       return true;
-    } else if (column > this.state.countOfColumnsAndRows - 1) {
+    } else if (column > this.state.targets[0].length - 1) {
       // -1 т.к. нач. и все послед массив у нас 0 1 2 3 4 - строк
-      //добавляем строку путём добавляения еще одного массива той же размерности
-      console.log("add new column");
-      console.log("Before: " + newTargets);
-      for (let i = 0; i < this.state.countOfColumnsAndRows; i++) {
-        newTargets[i].push(0);
+      //добавляем строку путём добавляния еще одного массива той же размерности
+      let tmpNewTargets = newTargets;
+      for (let i = 0; i < this.state.targets[0].length; i++) {
+        tmpNewTargets.push([]);
       }
-      console.log("After: " + newTargets);
+      for (let i = 0; i < this.state.targets[0].length; i++) {
+        tmpNewTargets[i].push(0);
+      }
+      newTargets = tmpNewTargets;
       return true;
     } else {
       return true;
@@ -57,25 +59,21 @@ class App extends Component {
       this.setState({ targets: newTargets });
     } else if (this.checkTargetIsExist(newTargets, row - 1, column)) {
       if (!newTargets[row - 1][column]) {
-        console.log("1");
         newTargets[row - 1][column] = 1;
         this.setState({ targets: newTargets });
         return;
       } else if (this.checkTargetIsExist(newTargets, row, column + 1)) {
         if (!newTargets[row][column + 1]) {
-          console.log("2");
           newTargets[row][column + 1] = 1;
           this.setState({ targets: newTargets });
           return;
         } else if (this.checkTargetIsExist(newTargets, row + 1, column)) {
           if (!newTargets[row + 1][column]) {
-            console.log("3");
             newTargets[row + 1][column] = 1;
             this.setState({ targets: newTargets });
             return;
           } else if (this.checkTargetIsExist(newTargets, row, column - 1)) {
             if (!newTargets[row][column - 1]) {
-              console.log("4");
               newTargets[row][column - 1] = 1;
               this.setState({ targets: newTargets });
               return;
@@ -108,17 +106,21 @@ class App extends Component {
             <header>Drag'n'Drop App</header>
             <div className="targetGrid">
               {targets.map((singleArrayTargets, i) => {
-                return singleArrayTargets.map((singleItem, j) => {
-                  return (
-                    <Target
-                      key={i + "" + j}
-                      idRow={i}
-                      idColumn={j}
-                      isEmpty={singleItem}
-                      handleClick={this.handleClick}
-                    />
-                  );
-                });
+                return (
+                  <div className="targetRow" id={i}>
+                    {singleArrayTargets.map((singleItem, j) => {
+                      return (
+                        <Target
+                          key={i + "" + j}
+                          idRow={i}
+                          idColumn={j}
+                          isEmpty={singleItem}
+                          handleClick={this.handleClick}
+                        />
+                      );
+                    })}
+                  </div>
+                );
               })}
             </div>
           </div>
